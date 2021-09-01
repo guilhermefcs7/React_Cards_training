34390891c1fc44cdef33e1523297eef840fb8710
Guilherme <guifalcao2010@hotmail.com>
2021-09-01 17:23:58 -0300

    counter react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 
-
+import Contador from "./components/contador/Contador";
 import Input from "./components/formulario/Input";
 import InderetaPai from "./components/comunicacao/InderetaPai";
 import DiretaPai from "./components/comunicacao/DiretaPai";
@@ -23,6 +23,12 @@ export default (props) => {
       <h1>Fundamentos React</h1>
 
       <div className="Cards">
+
+      <Card titulo="#12 - Contador" color="#424242">
+        
+        <Contador numeroInicial = {10}></Contador>
+         </Card>
+         
 
       <Card titulo="#11 - Componente Controlado" color="#E45F56">
         
